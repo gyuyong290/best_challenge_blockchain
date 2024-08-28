70b5177bab50935d4bbc270fedf3e7fad5bfbd4f
jiisuniui <dudndi9752@gmail.com>
2024-08-28 21:32:31 +0900

    constracts 구분선 주석

--- InspChain/packages/nextjs/contracts/deployedContracts.ts
+++ InspChain/packages/nextjs/contracts/deployedContracts.ts
@@ -7,6 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
 const deployedContracts = {
   31337: {
+    /* ------------------------------------------------------------------------------------------ */
     InspChain: {
       address: contractAddressList.InspChainAddress, //0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
       abi: [
@@ -321,6 +322,7 @@ const deployedContracts = {
       ],
       inheritedFunctions: {},
     },
+    /* ------------------------------------------------------------------------------------------ */
     YourContract: {
       address: contractAddressList.YourContractAddress, //0x5FbDB2315678afecb367f032d93F642f64180aa3
       abi: [
