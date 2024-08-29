d09578faf169f2c9f173ea78e9c62d189df9d56d
khjsla <rkdguswlsla@naver.com>
2024-08-29 16:06:59 +0900

    STYL: add icons

--- InspChain/packages/nextjs/app/page.tsx
+++ InspChain/packages/nextjs/app/page.tsx
@@ -14,12 +14,13 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-4xl font-bold">샘플A</span>
+            <span className="block text-4xl font-bold">블록체인 기반 스마트 점검 관리 시스템</span>
           </h1>
         </div>
         <div className="flex-grow bg-base-300 w-full mt-10 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
                 이거 컨트랙트 리스트로 변경해야함{" "}
                 <Link href="/debug" passHref className="link">
@@ -28,6 +29,7 @@ const Home: NextPage = () => {
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
                 이건 트랜잭션 리스트입니다{" "}
                 <Link href="/blockexplorer" passHref className="link">
--- InspChain/packages/nextjs/components/Header.tsx
+++ InspChain/packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -18,26 +18,32 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
   {
     label: "Contracts",
     href: "/debug",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
   {
     label: "Transaction List",
     href: "/blockexplorer",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
   {
     label: "승인",
     href: "/approve",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
   {
     label: "조회",
     href: "/query",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
   {
     label: "권한",
     href: "/grant",
+    icon: <MagnifyingGlassIcon className="h-4 w-4" />,
   },
 ];
 
@@ -108,8 +114,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">유유상종</span>
-            <span className="text-xs">블록체인 해커톤</span>
+            <span className="font-bold leading-tight">스마트 점검 관리 시스템</span>
+            <span className="text-xs">with BlockChain</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
