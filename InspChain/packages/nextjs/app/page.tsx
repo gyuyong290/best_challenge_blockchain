82974d0ce641823e1efb5b53ca18fb02e6ed3676
khjsla <rkdguswlsla@naver.com>
2024-08-29 20:23:14 +0900

    STYL: add icons

--- InspChain/packages/nextjs/app/page.tsx
+++ InspChain/packages/nextjs/app/page.tsx
@@ -31,27 +31,27 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <CheckCircleIcon className="h-8 w-8 fill-secondary" />
               <p>
-                승인 페이지{" "}
+                {" "}
                 <Link href="/debug" passHref className="link">
-                  Contract List
+                승인 페이지
                 </Link>{" "}
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <ClipboardDocumentListIcon className="h-8 w-8 fill-secondary" />
               <p>
-                점검 이력 페이지{" "}
+                {" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Transaction List
+                점검 이력 페이지
                 </Link>{" "}
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <ShieldCheckIcon className="h-8 w-8 fill-secondary" />
               <p>
-                권한 설정 페이지{" "}
+                {" "}
                 <Link href="/grant" passHref className="link">
-                  Grant
+                권한 설정 페이지
                 </Link>{" "}
               </p>
             </div>
